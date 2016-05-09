df17592597754afafc3ade93df817e5426ac33e5
sx5640 <sx5640@msn.com>
2016-05-09 18:02:21 -0400

    selector now works

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,27 +13,26 @@ class ArticlesController < ApplicationController
   private
 
   def article_selector
-    permit_params = params.permit(:start_time, :end_time, :search)
-    binding.pry
-    permit_params[:start_time] = DateTime.new(permit_params[:start_time]["(1i)"].to_i,
-                        permit_params[:start_time]["(2i)"].to_i,
-                        permit_params[:start_time]["(3i)"].to_i,
-                        permit_params[:start_time]["(4i)"].to_i,
-                        permit_params[:start_time]["(5i)"].to_i)
-    permit_params[:end_time] = DateTime.new(permit_params[:end_time]["(1i)"].to_i,
-                        permit_params[:end_time]["(2i)"].to_i,
-                        permit_params[:end_time]["(3i)"].to_i,
-                        permit_params[:end_time]["(4i)"].to_i,
-                        permit_params[:end_time]["(5i)"].to_i)
-    article_selected = []
+    permit_params = params.permit(:search, start_time: ["(1i)", "(2i)", "(3i)", "(4i)", "(5i)"], end_time: ["(1i)", "(2i)", "(3i)", "(4i)", "(5i)"])
     if permit_params[:start_time] && permit_params[:end_time]
-      Article.joins('keywords').where("articles.publication_time >= ? AND articles.publication_time <= ? and keywords.name = ?", permit_params[:start_time], permit_params[:end_time], permit_params[:search]).order(:publication_time)
-      binding.pry
-    else
-      Article.joins('keywords').where("keywords.name = ?", permit_params[:search]).order(:publication_time)
-      binding.pry
+      permit_params[:start_time] = DateTime.new(
+                          permit_params[:start_time]["(1i)"].to_i,
+                          permit_params[:start_time]["(2i)"].to_i,
+                          permit_params[:start_time]["(3i)"].to_i,
+                          permit_params[:start_time]["(4i)"].to_i,
+                          permit_params[:start_time]["(5i)"].to_i)
+      permit_params[:end_time] = DateTime.new(
+                          permit_params[:end_time]["(1i)"].to_i,
+                          permit_params[:end_time]["(2i)"].to_i,
+                          permit_params[:end_time]["(3i)"].to_i,
+                          permit_params[:end_time]["(4i)"].to_i,
+                          permit_params[:end_time]["(5i)"].to_i)
     end
-    return article_selected
+    if permit_params[:start_time] && permit_params[:end_time]
+      Article.joins(:keywords).where("articles.publication_time >= ? AND articles.publication_time <= ? and keywords.name = ?", permit_params[:start_time], permit_params[:end_time], permit_params[:search]).order(:publication_time)
+    else
+      Article.joins(:keywords).where("keywords.name = ?", permit_params[:search]).order(:publication_time)
+    end
   end
 
   def article_density(articles, start_time, end_time)
