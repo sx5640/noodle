8f433176a7f4029ecf4842b1b913123d9f95cb78
Peter La <peterla@peterla.com>
2016-05-10 18:01:08 -0400

    Fixed some syntax with missing semicolon and inconsistent indentation as well as added a comment.

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -9,29 +9,30 @@ $(document).on('ready', function() {
     var url = '/articles/search?utf8=%E2%9C%93&search=' + $('#search').val();
 
     $.ajax({
-        url: url,
-        type: 'GET',
-        dataType: 'json',
-        success: function(data) {
+      url: url,
+      type: 'GET',
+      dataType: 'json',
+      success: function(data) {
 
-          // Use Handlebars to compile our article summary templates and append the resulting html to the index page
+        // Use Handlebars to compile our article summary templates and append the resulting html to the index page
 
-          if (data) {
+        if (data) {
 
-            // First clear the article summary list
-            $('.article-summary-list').empty();
+          // First clear the article summary list
+          $('.article-summary-list').empty();
 
-            var source = $('#article-summary-list-item').html();
-            var template = Handlebars.compile(source);
+          // Compile the template with source HTML
+          var source = $('#article-summary-list-item').html();
+          var template = Handlebars.compile(source);
 
-            // Loop through each article and combine it with the template
-            for ( var i = 0; i < data.length; i++ ) {
-              var html = template(data[i]);
-              $('.article-summary-list').append(html);
-            }
+          // Loop through each article and combine it with the template
+          for ( var i = 0; i < data.length; i++ ) {
+            var html = template(data[i]);
+            $('.article-summary-list').append(html);
           }
         }
-    })
+      }
+    });
   });
 
 })
