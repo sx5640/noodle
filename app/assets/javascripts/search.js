32f19b3cbe7e3a537b0eeacea9fb85538d98ca52
Peter La <peterla@peterla.com>
2016-05-14 21:22:44 -0400

    Refactored code to add keywords in search.js into a separate function in timeline.js.

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -3,11 +3,8 @@ $(document).on('ready', function() {
   var global_zoneList;
 
   // AJAX call to submit search terms and display the article results
-
   $('.search-form').on('submit', function(eventObject) {
-
     eventObject.preventDefault();
-
     var url = '/articles/search?utf8=%E2%9C%93&search=' + $('#search').val();
 
     $.ajax({
@@ -17,43 +14,13 @@ $(document).on('ready', function() {
       success: function(data) {
 
         // Use Handlebars to compile our article summary templates and append the resulting html to the index page
-
         if (data) {
-
-          console.log(data);
 
           // First clear the article summary list
           $('#zone-list').empty();
 
           // Display Keywords for entire timeline
-          var htmlKeywords = "<div id='top-keywords-list'></div>";
-          $('#keywords-container').html(htmlKeywords);
-
-          if (data['keywords'] !== null) {
-            var keywordMax = data['keywords'].length < 10 ? data['keywords'].length : 10;
-
-            for (var i = 0; i < keywordMax; i++) {
-              var htmlKeyword = "<a href='/' class='top-keywords'>" + data['keywords'][i].keyword + "</a> ";
-              var fontWeight = '300';
-              if (data['keywords'][i].relevance > 10.0) {
-                fontWeight = '500';
-              } else
-              if (data['keywords'][i].relevance > 3.0) {
-                fontWeight = '400';
-              } else
-              if (data['keywords'][i].relevance > 2.0) {
-                fontWeight = '300';
-              } else
-              if (data['keywords'][i].relevance > 1.0) {
-                fontWeight = '200';
-              } else {
-                fontWeight = '100';
-              }
-              var appendObject = $(htmlKeyword);
-              appendObject.css('font-weight', fontWeight);
-              $('#top-keywords-list').append(appendObject);
-            }
-          }
+          replaceKeywords(data['keywords'], 10, $('#top-keywords-list'), 'top-keywords');
 
           // Compile the template with source HTML
           var sourceVisualization = $('#template-visualization').html();
@@ -80,44 +47,13 @@ $(document).on('ready', function() {
                 var dateDisplay = "";
 
                 // Display zone summary, eg. keywords, representative article, etc.
-
                 var htmlZone = templateZone(zone.article_list[0]);
                 $('#zone-list').append(htmlZone);
 
-                // Append first 5 keywords
-
-                // console.log(zone.keywords.length);
-                // var keywordMax2 = zone.keywords.length < 5 ? zone.keywords.length : 5;
-                // console.log(keywordMax2);
-
-                if (zone.keywords !== null) {
-                  var keywordMax2 = zone.keywords.length < 5 ? zone.keywords.length : 5;
-
-                  if (keywordMax2 > 0) {
-                    for (var j = 0; j < keywordMax2; j++) {
-                      var htmlZoneKeyword = "<li>" + zone.keywords[j].keyword + "</li>";
-                      var appendObject2 = $(htmlZoneKeyword);
-
-                      var fontWeight2 = '200';
-                      if (zone.keywords[j].relevance > 3.0) {
-                        fontWeight2 = '500';
-                      } else
-                      if (zone.keywords[j].relevance > 1.0) {
-                        fontWeight2 = '400';
-                      } else
-                      if (zone.keywords[j].relevance > .5) {
-                        fontWeight2 = '300';
-                      }
-                      appendObject2.css('font-weight', fontWeight2);
-                      $('.keywords').last().append(appendObject2);
-                    }
-                  } else {
-                    $('.keywords').last().append("<li>No keywords</li>");
-                  }
-                }
+                // Append first 3 keywords
+                replaceKeywords(zone.keywords, 3, $('.keywords').last(), 'top-keywords-zone', .25);
 
                 // Display zone hotness visualization (circle)
-
                 $('.zone').last().prepend(htmlVisualization);
                 visualizationDiv = $('.visualization').last();
 
@@ -127,11 +63,9 @@ $(document).on('ready', function() {
                 visualizationDiv.css('font-size', .75 + zone.hotness/4 + 'rem');
 
                 // Display year
-
                 $('.zone').last().prepend("<div class='zone-year'>" + end_date.getFullYear() + "</div>");
 
                 // Display month range of zone, eg. JUNE - JULY
-
                 if (start_month === end_month) {
                   dateDisplay = start_month;
                 } else {
@@ -146,13 +80,8 @@ $(document).on('ready', function() {
                 }
 
                 // Bind an eventhandler to each newly created zone to handle zooming
-
                 $('.zone').last().on('mouseover', function(eventObject) {
                   $(this).css('color', '#0096bf');
-
-                  // var originalSize = parseInt($(".visualization", this).css('width')) * 2;
-                  // console.log(originalSize);
-                  // $(".visualization", this).animate({ width: originalSize + "px", height: originalSize + "px" }, 100, "linear");
                 });
 
                 $('.zone').last().on('mouseout', function(eventObject) {
@@ -162,8 +91,8 @@ $(document).on('ready', function() {
                 $('.zone').last().on('click', function(eventObject) {
 
                   // Retrieve data for zone corresponding to click event
-                  var ind = $(this).index();
-                  var zone = data['zones'][ind];
+                  var zoneIndex = $(this).index();
+                  var zone = data['zones'][zoneIndex];
 
                   // Remove zone-list from DOM temporarily
                   global_zoneList = $('#zone-list').detach();
@@ -176,7 +105,13 @@ $(document).on('ready', function() {
                   for (var i = 0; i < zone.count; i++) {
                     var htmlArticle = templateZone(zone.article_list[i]);
                     $('.article-list').append(htmlArticle);
+
+                    // Append first 3 keywords
+                    replaceKeywords(zone.keywords, 3, $('.keywords').last(), 'top-keywords-zone', .25);
                   }
+
+                  // Show the top of the document instead of the bottom
+                  $(document).scrollTop(0);
                 });
               } else {
 
@@ -191,5 +126,4 @@ $(document).on('ready', function() {
       }
     });
   });
-
-})
+});
---
+++ app/assets/javascripts/timeline.js
@@ -0,0 +1,50 @@
+// Takes an array 'keywordsArray' of object type { keyword: string, relevance: float }
+// and appends the first 'numKeywords' keywords to the jQuery object 'jQuerySelector', which should select
+// an existing DOM element in the active document,
+// and styles each appended keyword's fontWeight based on its relevance value
+function replaceKeywords(keywordsArray, numKeywords, jQuerySelector, className, relevanceScaleFactor = 1.0) {
+  if (keywordsArray !== null) {
+
+    var htmlKeyword;
+    var relevance;
+    var fontWeight;
+    var keywordMax;
+
+    // First, empty the parent element since we're doing a replace
+    jQuerySelector.empty();
+
+    // Calculate the number of keywords to display
+    keywordMax = keywordsArray.length < numKeywords ? keywordsArray.length : numKeywords;
+
+    if (keywordMax > 0) {
+      // Loop through each keyword, attach it to the parent object specified by jQuerySelector,
+      // and style it's fontWeight css attribute according to the keyword's relevance.
+      for (var i = 0; i < keywordMax; i++) {
+        htmlKeyword = "<li><a href='/' class='" + className + "'>" + keywordsArray[i].keyword + "</a></li>";
+        relevance = keywordsArray[i].relevance;
+        fontWeight = '100';
+        if (relevance > 10.0 * relevanceScaleFactor) {
+          fontWeight = '500';
+        } else if (relevance > 3.0 * relevanceScaleFactor) {
+          fontWeight = '400';
+        } else if (relevance > 2.0 * relevanceScaleFactor) {
+          fontWeight = '300';
+        } else if (relevance > 1.0 * relevanceScaleFactor) {
+          fontWeight = '200';
+        }
+        var appendObject = $(htmlKeyword);
+        appendObject.find('a').css('font-weight', fontWeight);
+        jQuerySelector.append(appendObject);
+      }
+    } else {
+      htmlKeyword = "<li><a href='/' class='" + className + "'>No Keywords</a></li>";
+      appendObject = $(htmlKeyword);
+      appendObject.find('a').css('font-weight', '400');
+      jQuerySelector.append(appendObject);
+    }
+
+    return true;
+  } else {
+    return false;
+  }
+}
