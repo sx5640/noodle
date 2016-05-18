b48fdb9fe80dcfc111782bc5015779e735e99fb7
sx5640 <sx5640@msn.com>
2016-05-18 19:01:10 -0400

    click into saved timelines from user profile

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -34,23 +34,13 @@ class ArticlesController < ApplicationController
   # defining a method to clean up the params
   def permit_params
     # permit only search terms and time frames
-    permitted_params = params.permit(:search, start_time: ["(1i)", "(2i)", "(3i)", "(4i)", "(5i)"], end_time: ["(1i)", "(2i)", "(3i)", "(4i)", "(5i)"])
+    permitted_params = params.permit(:search, :start_time, :end_time)
     # if there is a timeframe, clean up the timeframe and turn into DateTime object
     if permitted_params[:start_time] && permitted_params[:end_time]
-      permitted_params[:start_time] = DateTime.new(
-        permitted_params[:start_time]["(1i)"].to_i,
-        permitted_params[:start_time]["(2i)"].to_i,
-        permitted_params[:start_time]["(3i)"].to_i,
-        permitted_params[:start_time]["(4i)"].to_i,
-        permitted_params[:start_time]["(5i)"].to_i
-      )
-      permitted_params[:end_time] = DateTime.new(
-        permitted_params[:end_time]["(1i)"].to_i,
-        permitted_params[:end_time]["(2i)"].to_i,
-        permitted_params[:end_time]["(3i)"].to_i,
-        permitted_params[:end_time]["(4i)"].to_i,
-        permitted_params[:end_time]["(5i)"].to_i
-      )
+      permitted_params[:start_time] = DateTime.parse(
+        permitted_params[:start_time])
+      permitted_params[:end_time] = DateTime.parse(
+        permitted_params[:end_time])
     end
     return permitted_params
   end
