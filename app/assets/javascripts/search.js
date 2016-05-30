45631762c0dabba88fe4c663b1464e54a8441e14
Peter La <peterla@peterla.com>
2016-05-30 16:59:52 -0400

    Refactored search.js and fixed comments so that it is better organized and easier to understand.

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,18 +1,43 @@
 $(document).on('ready page:load', function() {
 
+  //
+  // View stack and corresponding data stack that describes multiple levels of timelines
+  //
   var global_views = [];
-  var global_data = [];
+  var global_data  = [];
 
-  // AJAX call to submit search terms and display the article results
-  var newSearch = function(search_string, start_time, end_time) {
+  //
+  // Get saved timeline, populated by the user profile
+  //
+  var savedTimeline = $('#search-section').data();
+
+  //
+  // Starting point 1 - handle event: search submit
+  //
+  $('.search-form').on('submit', function(eventObject) {
+    eventObject.preventDefault();
+    newSearch( $('.search-form #search').val() );
+  });
+
+  //
+  // Starting point 2 - handle returning to this page via user profile page by displaying the timeline described by
+  // the search-section's data attribute, if present
+  //
+  if (savedTimeline && savedTimeline.searchString) {
+    newSearch(savedTimeline.searchString, savedTimeline.startTime, savedTimeline.endTime);
+  }
+
+  // Perform a search via AJAX request
+  function newSearch(search_string, start_time, end_time) {
     searchFunction(search_string, start_time, end_time, false);
   };
 
-  var subSearch = function(search_string, start_time, end_time) {
+  // Perform a subsearch via AJAX request
+  function subSearch(search_string, start_time, end_time) {
     searchFunction(search_string, start_time, end_time, true);
   };
 
-  var searchFunction = function(search_string, start_time, end_time, is_sub_search) {
+  function searchFunction(search_string, start_time, end_time, is_sub_search) {
     if (start_time && end_time) {
       var url = '/articles/search?utf8=%E2%9C%93&search=' + search_string + '&start_time=' + start_time + '&end_time=' + end_time;
     }
@@ -35,7 +60,7 @@ $(document).on('ready page:load', function() {
         }
 
         // Build timeline, display it and push it onto stack along with its data
-        pushTimelineView(data);
+        displayTimelineView(data);
 
         // Render minimap
         minimap.render(data['zones']);
@@ -44,155 +69,12 @@ $(document).on('ready page:load', function() {
     });
   }
 
-  // If there are data in the data attribute, do an automatic search
-  presetData = $('#search-section').data();
-
-  if (presetData && presetData.searchString) {
-    newSearch(presetData.searchString, presetData.startTime, presetData.endTime);
-  }
-
-  // Submit search terms and send AJAX call
-  $('.search-form').on('submit', function(eventObject) {
-    eventObject.preventDefault();
-    newSearch( $('.search-form #search').val() );
-  });
-
-  function pushTimelineView(data) {
-
-    // Use Handlebars to compile our article summary templates and append the resulting html to the index page
-    if (data) {
-
-      var firstNonEmptyZoneDisplayed = false;
-
-      // Remove currently displayed timeline from DOM so we can attach the new timeline view
-      $('#timeline').remove();
-
-      var htmlTimeline = '<div id="timeline"><ul id="zone-list"></ul></div>';
-      var root = $('.content-container').html(htmlTimeline);
-
-      // Display Keywords for entire timeline
-      timeline.replaceKeywords(data['keywords'], 10, $('#top-keywords-list'), 'top-keywords');
-
-      // Compile the template with source HTML
-      var sourceVisualization = $('#template-visualization').html();
-      var templateVisualization = Handlebars.compile(sourceVisualization);
-
-      var sourceZone = $('#template-zone').html();
-      var templateZone = Handlebars.compile(sourceZone);
-
-      // Display Timeline title
-      var htmlTimelineTitle = '<div id="timeline-header"><h1 class="timeline-title">Timeline</h1></div>';
-      $('#timeline').prepend(htmlTimelineTitle);
-
-      if (data.user && data.user.saved_this_timeline === false) {
-
-        var htmlTimelineSave = '<h1 id="save-timeline-button"><a id="save-timeline" href="">*</a></h1>';
-        $('#timeline-header').append(htmlTimelineSave);
-
-        // Add event handler for saving the timeline to the user model
-        $('#save-timeline').on('click', function(eventObject) {
-          eventObject.preventDefault();
-
-          var url = '/users/' + data.user.user_id + '/saved_timelines';
-
-          $.ajax({
-            url: url,
-            type: 'POST',
-            dataType: 'json',
-            data: {
-              search_string: data.search_info.search_string,
-              start_time: data.search_info.start_time,
-              end_time: data.search_info.end_time
-            },
-            success: function(data) {
-              console.log(data);
-              $('#save-timeline-button').html('<h1 class="timeline-saved-message">saved</h1>');
-            }
-          });
-        });
-      }
-
-      // Loop through each zone and combine it with the templates
-      for ( var i = data['zones'].length - 1; i >= 0 ; i-- ) {
-        console.log(data['zones'][i]);
-
-        var zone = data['zones'][i];
-
-        if (zone !== null) {
-          if (zone.count > 0) {
-            var htmlVisualization = templateVisualization(zone);
-            var size = 25 + zone.hotness * 25;
-            var visualizationDiv;
-            var start_date = new Date(zone.start_time);
-            var end_date = new Date(zone.end_time);
-            var months = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
-            var start_month = months[start_date.getMonth()];
-            var end_month = months[end_date.getMonth()];
-            var dateDisplay = "";
-
-            // Display zone summary, eg. keywords, representative article, etc.
-            var htmlZone = templateZone(zone.article_list[0]);
-            $('#zone-list').append(htmlZone);
-
-            // Append first 3 keywords
-            timeline.replaceKeywords(zone.keywords, 3, $('.keywords').last(), 'top-keywords-zone', .25);
-
-            // Display number of articles
-            var htmlNumberOfArticles = zone.article_list.length;
-
-            if (zone.article_list.length === 0 || zone.article_list.length > 1) {
-              htmlNumberOfArticles += ' stories';
-            } else {
-              htmlNumberOfArticles += ' story';
-            }
-            
-            $('.number-of-articles').last().html(htmlNumberOfArticles);
-
-            // Display zone hotness visualization (circle)
-            $('.zone').last().prepend(htmlVisualization);
-            visualizationDiv = $('.visualization').last();
-
-            visualizationDiv.css('width', size + 'px');
-            visualizationDiv.css('height', size + 'px');
-            visualizationDiv.css('opacity', .35 + zone.hotness * .05);
-            visualizationDiv.css('font-size', .75 + zone.hotness/4 + 'rem');
-
-            // Display year
-            $('.zone').last().prepend("<div class='zone-year'>" + end_date.getFullYear() + "</div>");
-
-            // Display month range of zone, eg. JUNE - JULY
-            if (start_month === end_month) {
-              dateDisplay = start_month;
-            } else {
-              dateDisplay = start_month + " - " + end_month;
-            }
-            dateDisplay = dateDisplay.toUpperCase();
-            $('.zone').last().prepend("<div class='zone-month'>" + dateDisplay + "</div>");
-
-            // Display vertical zone connector line, except for the first zone
-            if (firstNonEmptyZoneDisplayed) {
-              $('.zone').last().prepend("<div class='zone-connector'></div>");
-            }
-
-            if (!firstNonEmptyZoneDisplayed) {
-              firstNonEmptyZoneDisplayed = true;
-            }
-          } else {
-
-            // If there are no articles, create an empty list item with class zone
-            // This is important so that we can use the zone li index to correctly select the right right zone data
-            var htmlZone = "<li class='zone'></li>";
-            $('#zone-list').append(htmlZone);
-          }
-        }
-      }
-
-      // Push the new view and data
-
-      global_views.push($('#timeline'));
-      global_data.push(data);
-    }
-  }
+  //
+  // Handle event: click on keyword
+  //
+  // Trigger an AJAX call to do a subsearch
+  //
+  $('#top-keywords-list').on('click', clickKeyword);
 
   function clickKeyword(eventObject) {
     eventObject.preventDefault();
@@ -200,10 +82,11 @@ $(document).on('ready page:load', function() {
     subSearch(global_data[global_data.length - 1].search_info.search_string + "|" + $(eventObject.target).text());
   }
 
-  $('#top-keywords-list').on('click', clickKeyword);
-  // Click event handler to change views when a user clicks on a zone
-  // If the number of articles in the zone is < 20, an articles view will be displayed
-  // Otherwise, another timeline will be displayed, the data for that timeline will be via an ajax call
+  //
+  // Handle event: click on zone
+  //
+  // Trigger an AJAX call to get another timeline, or display articles list (< 20 articles in zone)
+  //
   $('.content-container').on('click', function(eventObject) {
 
     var matchedZones = $(eventObject.target).closest('.zone');
@@ -232,7 +115,7 @@ $(document).on('ready page:load', function() {
           dataType: 'json',
           success: function(data) {
             // Build timeline, display it and push it onto stack along with its data
-            pushTimelineView(data);
+            displayTimelineView(data);
 
             // Render minimap
             minimap.render(data['zones']);
@@ -281,19 +164,17 @@ $(document).on('ready page:load', function() {
         minimap.render(null);
 
         // Scroll the document to the start of content
-        scrollToContentContainerTop();
+        var documentHeight = $(document).height();
+        var articleListHeight = $('.content-container').outerHeight();
+        var headerHeight = documentHeight - articleListHeight + 60;
+        $(document).scrollTop(headerHeight);
       }
     }
   });
 
-  function scrollToContentContainerTop() {
-      var documentHeight = $(document).height();
-      var articleListHeight = $('.content-container').outerHeight();
-      var headerHeight = documentHeight - articleListHeight + 60;
-      $(document).scrollTop(headerHeight);
-  }
-
-  // Add event handler to go back to timeline via AJAX
+  //
+  // Handle event: click to go back up a level
+  //
   $('#timeline-nav').on('click', function(eventObject) {
     eventObject.preventDefault();
 
@@ -317,10 +198,190 @@ $(document).on('ready page:load', function() {
     }
   });
 
+  //
+  // Handle event: document scroll
+  //
+  // Update the minimap to reflect latest position
+  //
   $(window).scroll(function() {
     updateMinimap();
   });
 
+  //
+  // Handle event: mouse over on minimap
+  //
+  $('#minimap-container').on('mouseover', function(eventObject) {
+    var source = eventObject.target;
+
+    if ($(source).hasClass('circle')) {
+      $('.minimap-circle-hover').removeClass('minimap-circle-hover');
+      $(source).toggleClass('minimap-circle-hover');
+    }
+  });
+
+  //
+  // Handle event: mouse out on minimap
+  //
+  $('#minimap-container').on('mouseout', function(eventObject) {
+    $('.minimap-circle-hover').removeClass('minimap-circle-hover');
+    updateMinimap();
+  });
+
+  //
+  // Handle event: click on minimap
+  //
+  $('#minimap-container').on('click', function(eventObject) {
+    var offset = $(this).offset();
+    var minimapY = eventObject.pageY - offset.top;
+    var minimapHeight = 500;
+    var zoneListHeight = $('#zone-list').outerHeight();
+    var newY = minimapY/minimapHeight * zoneListHeight;
+
+    $(document).scrollTop(newY);
+  });
+
+  //
+  // View: display timeline described by the data object
+  //
+  function displayTimelineView(data) {
+
+    // Use Handlebars to compile our article summary templates and append the resulting html to the index page
+    if (data) {
+
+      var firstNonEmptyZoneDisplayed = false;
+
+      // Remove currently displayed timeline from DOM so we can attach the new timeline view
+      $('#timeline').remove();
+
+      var htmlTimeline = '<div id="timeline"><ul id="zone-list"></ul></div>';
+      var root = $('.content-container').html(htmlTimeline);
+
+      // Display Keywords for entire timeline
+      timeline.replaceKeywords(data['keywords'], 10, $('#top-keywords-list'), 'top-keywords');
+
+      // Compile the template with source HTML
+      var sourceVisualization = $('#template-visualization').html();
+      var templateVisualization = Handlebars.compile(sourceVisualization);
+
+      var sourceZone = $('#template-zone').html();
+      var templateZone = Handlebars.compile(sourceZone);
+
+      // Display Timeline title
+      var htmlTimelineTitle = '<div id="timeline-header"><h1 class="timeline-title">Timeline</h1></div>';
+      $('#timeline').prepend(htmlTimelineTitle);
+
+      if (data.user && data.user.saved_this_timeline === false) {
+
+        var htmlTimelineSave = '<h1 id="save-timeline-button"><a id="save-timeline" href="">*</a></h1>';
+        $('#timeline-header').append(htmlTimelineSave);
+
+        // Add event handler for saving the timeline to the user model
+        $('#save-timeline').on('click', function(eventObject) {
+          eventObject.preventDefault();
+
+          var url = '/users/' + data.user.user_id + '/saved_timelines';
+
+          $.ajax({
+            url: url,
+            type: 'POST',
+            dataType: 'json',
+            data: {
+              search_string: data.search_info.search_string,
+              start_time: data.search_info.start_time,
+              end_time: data.search_info.end_time
+            },
+            success: function(data) {
+              $('#save-timeline-button').html('<h1 class="timeline-saved-message">saved</h1>');
+            }
+          });
+        });
+      }
+
+      // Loop through each zone and combine it with the templates
+      for ( var i = data['zones'].length - 1; i >= 0 ; i-- ) {
+        console.log(data['zones'][i]);
+
+        var zone = data['zones'][i];
+
+        if (zone !== null) {
+          if (zone.count > 0) {
+            var htmlVisualization = templateVisualization(zone);
+            var size = 25 + zone.hotness * 25;
+            var visualizationDiv;
+            var start_date = new Date(zone.start_time);
+            var end_date = new Date(zone.end_time);
+            var months = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
+            var start_month = months[start_date.getMonth()];
+            var end_month = months[end_date.getMonth()];
+            var dateDisplay = "";
+
+            // Display zone summary, eg. keywords, representative article, etc.
+            var htmlZone = templateZone(zone.article_list[0]);
+            $('#zone-list').append(htmlZone);
+
+            // Append first 3 keywords
+            timeline.replaceKeywords(zone.keywords, 3, $('.keywords').last(), 'top-keywords-zone', .25);
+
+            // Display number of articles
+            var htmlNumberOfArticles = zone.article_list.length;
+
+            if (zone.article_list.length === 0 || zone.article_list.length > 1) {
+              htmlNumberOfArticles += ' stories';
+            } else {
+              htmlNumberOfArticles += ' story';
+            }
+
+            $('.number-of-articles').last().html(htmlNumberOfArticles);
+
+            // Display zone hotness visualization (circle)
+            $('.zone').last().prepend(htmlVisualization);
+            visualizationDiv = $('.visualization').last();
+
+            visualizationDiv.css('width', size + 'px');
+            visualizationDiv.css('height', size + 'px');
+            visualizationDiv.css('opacity', .35 + zone.hotness * .05);
+            visualizationDiv.css('font-size', .75 + zone.hotness/4 + 'rem');
+
+            // Display year
+            $('.zone').last().prepend("<div class='zone-year'>" + end_date.getFullYear() + "</div>");
+
+            // Display month range of zone, eg. JUNE - JULY
+            if (start_month === end_month) {
+              dateDisplay = start_month;
+            } else {
+              dateDisplay = start_month + " - " + end_month;
+            }
+            dateDisplay = dateDisplay.toUpperCase();
+            $('.zone').last().prepend("<div class='zone-month'>" + dateDisplay + "</div>");
+
+            // Display vertical zone connector line, except for the first zone
+            if (firstNonEmptyZoneDisplayed) {
+              $('.zone').last().prepend("<div class='zone-connector'></div>");
+            }
+
+            if (!firstNonEmptyZoneDisplayed) {
+              firstNonEmptyZoneDisplayed = true;
+            }
+          } else {
+
+            // If there are no articles, create an empty list item with class zone
+            // This is important so that we can use the zone li index to correctly select the right right zone data
+            var htmlZone = "<li class='zone'></li>";
+            $('#zone-list').append(htmlZone);
+          }
+        }
+      }
+
+      // Push the new view and data
+
+      global_views.push($('#timeline'));
+      global_data.push(data);
+    }
+  }
+  
+  //
+  // View: udpate the minimap to reflect the latest scroll position
+  //
   function updateMinimap() {
     var documentHeight = $(document).height();
     var windowHeight = $(window).height();
@@ -345,37 +406,4 @@ $(document).on('ready page:load', function() {
       circleInFocus.toggleClass('minimap-circle-pop');
     }
   }
-
-  // Event handlers to detect mouseover and click events on minimap
-  $('#minimap-container').on('mouseover', function(eventObject) {
-    var source = eventObject.target;
-
-    if ($(source).hasClass('circle')) {
-      $('.minimap-circle-hover').removeClass('minimap-circle-hover');
-      $(source).toggleClass('minimap-circle-hover');
-    }
-  });
-
-  $('#minimap-container').on('mouseout', function(eventObject) {
-    $('.minimap-circle-hover').removeClass('minimap-circle-hover');
-    updateMinimap();
-  });
-
-  $('#minimap-container').on('click', function(eventObject) {
-    var offset = $(this).offset();
-    var minimapX = eventObject.pageX - offset.left;
-    var minimapY = eventObject.pageY - offset.top;
-    var minimapHeight = 500;
-    var documentHeight = $(document).height();
-    var windowHeight = $(window).height();
-    var zoneListHeight = $('#zone-list').outerHeight();
-    var headerHeight = documentHeight - zoneListHeight;
-
-    var newY = minimapY/minimapHeight * zoneListHeight;// + headerHeight;
-
-    console.log('newY ', newY);
-
-    $(document).scrollTop(newY);
-    // $(document).animate({ scrollTop:newY }, 'slow');
-  });
 });
