1b9523d74d7b7216bb9d84655c80278ddd3a5604
Peter La <peterla@peterla.com>
2016-06-01 15:45:00 -0400

    Added basic intro animation to Three.js visualization.

--- app/assets/javascripts/visualization.js
+++ app/assets/javascripts/visualization.js
@@ -6,13 +6,13 @@ var visualization = (function() {
   var camera;
   var renderer;
   var boxMeshes;
-  // var boxGeometry;
-  // var boxMaterial;
   var groundPlane;
   var groundPlaneGeometry;
   var groundPlaneMaterial;
   var ambientLight;
   var directionalLight;
+  var requestID;
+  var visualizationRendered;
 
   return {
 
@@ -20,6 +20,9 @@ var visualization = (function() {
     // View: Initializes Three.js
     //
     init: function() {
+      // Set visualization rendered flag to false
+      visualizationRendered = false;
+
       // Query container element for dimensions to set our Three.js canvas to
       width = $( '#visualization-container' ).width();
       height = $( '#visualization-container' ).height();
@@ -48,26 +51,21 @@ var visualization = (function() {
       scene.add(groundPlane);
 
       // Create lights
-      ambientLight = new THREE.AmbientLight( 0x5f5f5f );
+      ambientLight = new THREE.AmbientLight( 0x888888 );
       scene.add(ambientLight);
 
       directionalLight = new THREE.DirectionalLight( 0xffffff, 1 );
       directionalLight.position.set(500, 1000, 250);
       directionalLight.position.multiplyScalar(1.3);
-
       directionalLight.castShadow = true;
       directionalLight.shadowCameraVisible = true;
-
       directionalLight.shadowMapWidth = 2048;
       directionalLight.shadowMapHeight = 2048;
-
       var d = 2000;
-
       directionalLight.shadowCameraLeft = -d;
       directionalLight.shadowCameraRight = d;
       directionalLight.shadowCameraTop = d;
       directionalLight.shadowCameraBottom = -d;
-
       directionalLight.shadowCameraFar = 10000;
       directionalLight.shadowDarkness = 0.2;
       scene.add(directionalLight);
@@ -77,9 +75,32 @@ var visualization = (function() {
       $( '#visualization-container' ).append( renderer.domElement );
 
       // Define render callback animation function and pass it to requestAnimationFrame()
+      var animationFrameCounter = 1;
+      var animationFrameMax = 30;
       var render = function () {
-        requestAnimationFrame( render );
-        renderer.render( scene, camera );
+          requestID = requestAnimationFrame( render );
+
+          if (!visualizationRendered) {
+            if (boxMeshes) {
+              for (var i = 0; i < boxMeshes.length; i++) {
+                var cube = boxMeshes[i].mesh;
+                var targetHeight = boxMeshes[i].targetHeight;
+                var scale = animationFrameCounter/animationFrameMax;
+                var sinScale = Math.sin(scale*Math.PI/2);
+                sinScale = sinScale * sinScale;
+                cube.scale.setY(sinScale);
+                cube.position.setY(targetHeight * sinScale / 2);
+                // console.log(boxMesh);
+              }
+              animationFrameCounter++;
+              if (animationFrameCounter > animationFrameMax) {
+                visualizationRendered = true;
+                animationFrameCounter = 1;
+              }
+            }
+          }
+
+          renderer.render( scene, camera );
       };
 
       // Kick off the rendering process
@@ -90,6 +111,8 @@ var visualization = (function() {
     // View: Renders a 3D visualization of the given zones array to #visualization-container
     //
     render: function(zones) {
+      // Reset visualizationRendered flag so that it animates into view
+      visualizationRendered = false;
 
       // Remove all previously added objects from the scene, other than camera
       for (let i = scene.children.length - 1; i >= 0 ; i--) {
@@ -116,7 +139,7 @@ var visualization = (function() {
         cube.receiveShadow = true;
         cube.scale.setX(0.033);
         cube.position.set( (numZones/2 - i) * spacing, verticalSize/2, 0 );
-        boxMeshes.push(cube); // Stash box for later use
+        boxMeshes.push( { mesh: cube, targetHeight: verticalSize } ); // Stash box for later use
         scene.add( cube );
       }
     }
